1b6e574818673b6e00b5eb7038ec4202b740a62f
Brian <Bhensley811@gmail.com>
2022-09-25 15:37:44 -0400

    commit tree 1d6238705c78cbaf2293aac905123f91bb101efa

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-// Add your code here
+function submitData(name, email) {
+  function renderData(data) {
+    const newDiv = document.createElement("li");
+    newDiv.className = "user_data";
+    newDiv.innerHTML = data;
+  }
+
+  let formData = {
+    name,
+    email,
+  };
+
+  const configurationalObject = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/json",
+    },
+    body: JSON.stringify(formData),
+  };
+
+  return fetch("http://localhost:3000/users", configurationalObject)
+    .then((res) => res.json())
+    .then((obj) => renderData(obj));
+}
